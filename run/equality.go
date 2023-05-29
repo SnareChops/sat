ad7df0a6f1dcfb3fb369f63f4929444affdcb63c
SnareChops <snare_chops@yahoo.com>
2023-05-29 04:57:17 -0400

    Added support for equality comparing objects and arrays Added parsing of json in `get` request responses

--- run/equality.go
+++ run/equality.go
@@ -9,17 +9,17 @@ import (
 	"github.com/SnareChops/sat/types"
 )
 
-func run_equality(exp expression.Equality, scope *types.Scope) (types.Primitive, error) {
+func run_equality(exp expression.Equality, scope *types.Scope) (primitive.Boolean, error) {
 	if *options.Trace {
 		fmt.Printf("run_equality\n\t%v\n\t%v\n---\n", exp, scope)
 	}
 	left_prim, err := run_expression(exp.Left, scope)
 	if err != nil {
-		return nil, err
+		return primitive.Boolean{}, err
 	}
 	right_prim, err := run_expression(exp.Right, scope)
 	if err != nil {
-		return nil, err
+		return primitive.Boolean{}, err
 	}
 	switch left_prim.(type) {
 	case primitive.Void:
@@ -51,9 +51,19 @@ func run_equality(exp expression.Equality, scope *types.Scope) (types.Primitive,
 			return primitive.Boolean{false}, nil
 		}
 	case primitive.Object:
-		panic("todo")
+		switch right_prim.(type) {
+		case primitive.Object:
+			return object_equality(left_prim.(primitive.Object), right_prim.(primitive.Object), scope)
+		default:
+			return primitive.Boolean{false}, nil
+		}
 	case primitive.Array:
-		panic("todo")
+		switch right_prim.(type) {
+		case primitive.Array:
+			return array_equality(left_prim.(primitive.Array), right_prim.(primitive.Array), scope)
+		default:
+			return primitive.Boolean{false}, nil
+		}
 	case primitive.Multi:
 		panic("todo")
 	case primitive.Process:
@@ -61,4 +71,40 @@ func run_equality(exp expression.Equality, scope *types.Scope) (types.Primitive,
 	default:
 		panic("unknown equality comparison")
 	}
+}
+
+func object_equality(left, right primitive.Object, scope *types.Scope) (primitive.Boolean, error) {
+	if len(left.Value) != len(right.Value) {
+		return primitive.Boolean{false}, nil
+	}
+	for key, lvalue := range left.Value {
+		rvalue, ok := right.Value[key]
+		if !ok {
+			return primitive.Boolean{false}, nil
+		}
+		b, err := run_equality(expression.Equality{lvalue.Location(), lvalue, rvalue}, scope)
+		if err != nil {
+			return primitive.Boolean{}, err
+		}
+		if !b.Value {
+			return primitive.Boolean{false}, nil
+		}
+	}
+	return primitive.Boolean{true}, nil
+}
+
+func array_equality(left, right primitive.Array, scope *types.Scope) (primitive.Boolean, error) {
+	if len(left.Value) != len(right.Value) {
+		for i, lvalue := range left.Value {
+			rvalue := right.Value[i]
+			b, err := run_equality(expression.Equality{lvalue.Location(), lvalue, rvalue}, scope)
+			if err != nil {
+				return primitive.Boolean{}, err
+			}
+			if !b.Value {
+				return primitive.Boolean{false}, nil
+			}
+		}
+	}
+	return primitive.Boolean{true}, nil
 }
--- run/get.go
+++ run/get.go
@@ -1,6 +1,7 @@
 package run
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -31,8 +32,76 @@ func run_get(get expression.Get, scope *types.Scope) (types.Primitive, error) {
 	if err != nil {
 		return nil, RunError(get.Loc, err.Error())
 	}
+	var result types.Primitive
+	result = primitive.String{string(body)}
+	if body[0] == '{' {
+		unknown := map[string]interface{}{}
+		json.Unmarshal(body, &unknown)
+		r, err := parse_json_object(get.Loc, unknown)
+		if err != nil {
+			return nil, err
+		}
+		result = r
+	} else if body[0] == '[' {
+		unknown := []interface{}{}
+		json.Unmarshal(body, &unknown)
+		r, err := parse_json_array(get.Loc, unknown)
+		if err != nil {
+			return nil, err
+		}
+		result = r
+	}
+
 	return primitive.Multi{[]types.Primitive{
 		primitive.Number{float32(res.StatusCode)},
-		primitive.String{string(body)},
+		result,
 	}}, err
+}
+
+func parse_json_object(loc types.Location, object map[string]interface{}) (primitive.Object, error) {
+	prims := map[string]types.Expression{}
+	for key, value := range object {
+		parsed, err := parse_unknown(loc, value)
+		if err != nil {
+			return primitive.Object{}, err
+		}
+		prims[key] = expression.Primitive{types.Location{}, parsed}
+	}
+	return primitive.Object{prims}, nil
+}
+
+func parse_json_array(loc types.Location, array []interface{}) (primitive.Array, error) {
+	prims := []types.Expression{}
+	for _, value := range array {
+		parsed, err := parse_unknown(loc, value)
+		if err != nil {
+			return primitive.Array{}, err
+		}
+		prims = append(prims, expression.Primitive{types.Location{}, parsed})
+	}
+	return primitive.Array{prims}, nil
+}
+
+func parse_unknown(loc types.Location, unknown interface{}) (types.Primitive, error) {
+	switch unknown.(type) {
+	case map[string]interface{}:
+		return parse_json_object(loc, unknown.(map[string]interface{}))
+	case []interface{}:
+		return parse_json_array(loc, unknown.([]interface{}))
+	case string:
+		return primitive.String{unknown.(string)}, nil
+	case int:
+		return primitive.Number{float32(unknown.(int))}, nil
+	case int64:
+		return primitive.Number{float32(unknown.(int64))}, nil
+	case float32:
+		return primitive.Number{unknown.(float32)}, nil
+	case float64:
+		return primitive.Number{float32(unknown.(float64))}, nil
+	case bool:
+		return primitive.Boolean{unknown.(bool)}, nil
+	default:
+		return nil, RunError(loc, "unexpected value while parsing json")
+	}
+
 }
